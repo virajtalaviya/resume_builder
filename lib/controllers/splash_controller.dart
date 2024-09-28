be84542ae146fac327c25e13bce29dd9187901ff
virajtalaviya <virajtalaviya447@gmail.com>
2024-09-28 22:03:36 +0530

    text fields added

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -1,7 +1,11 @@
 import 'package:get/get.dart';
+import 'package:resume_builder/screens/home_screen.dart';
 
 class SplashController extends GetxController {
-  void initAdministration() {}
+  void initAdministration() async {
+    await Future.delayed(const Duration(seconds: 3));
+    Get.to(() => const HomeScreen());
+  }
 
   @override
   void onInit() {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:resume_builder/components/common_textfield.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fill the form"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+        child: Column(
+          children: [
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Name",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Designation",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Phone no.",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Email",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Address",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "LinkedIn",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Github",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Profile",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Skills",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Languages",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Profile",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Work Exp. (Company Name)",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Duration",
+            ),
+            CommonTextfield(
+              textEditingController: TextEditingController(),
+              hintText: "Work Exp. (Description)",
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
